c737e18c82ecaed0e60f591ff048c2b76c953c0b
Javier Ailbirt <jailbirt@interactar.com>
2016-04-08 03:00:02 +0000

    Docker RMI all images

--- docker/dockerRMImages.sh
+++ docker/dockerRMImages.sh
@@ -1,1 +1,4 @@
-for i in $(docker images|tr -s " "|cut -d' ' -f3|grep -v IMAGE); do docker rmi $i ; done
+echo killing dockers
+for i in $(docker ps|tr -s " "|cut -d' ' -f1|grep -v CON); do echo killing $i && docker kill $i ; done
+echo deleting dockers
+for i in $(docker images|tr -s " "|cut -d' ' -f3|grep -v IMAGE); do echo Deleting $i && docker rmi -f $i ; done
