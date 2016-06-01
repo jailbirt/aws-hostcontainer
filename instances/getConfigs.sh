e212e77a0cc11fa468f7fee52b863bb1f7249137
Juan Zurano <jgzurano@interactar.com>
2016-06-01 03:32:06 +0000

    sudo for chown

--- instances/getConfigs.sh
+++ instances/getConfigs.sh
@@ -4,4 +4,4 @@ aws='/usr/bin/aws'
 source $scriptsPath/instances/instanceVars.sh --printvars
 echo Get Your Configurations $aws s3 sync s3://$configurationsBucket /home/ubuntu/configs
 $aws s3 sync s3://$configurationsBucket /home/ubuntu/configs
-chown -R ubuntu.ubuntu /home/ubuntu/configs
+sudo chown -R ubuntu.ubuntu /home/ubuntu/configs
