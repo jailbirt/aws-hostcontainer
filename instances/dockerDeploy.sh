630486b0a2e7e2659efd2f3cfa6ddec2d5253ea9
Juan Zurano <jgzurano@interactar.com>
2016-06-13 15:26:32 +0000

    background compose

--- instances/dockerDeploy.sh
+++ instances/dockerDeploy.sh
@@ -13,4 +13,5 @@ $compose -f docker-$dockerEnv-compose.yml down
 for i in $(docker ps -a|cut -d ' ' -f1|grep -v CONTA) ; do docker rm $i ; done
 echo "starting docker-$dockerEnv-compose.yml"
 #I'm not interested in stdout. Hopefully I ll configure using other way.
-(nohup $compose -f docker-$dockerEnv-compose.yml up > /dev/null 2>&1 &)
+#(nohup $compose -f docker-$dockerEnv-compose.yml up > /dev/null 2>&1 &)
+($compose -f docker-$dockerEnv-compose.yml up &> /dev/null &)
