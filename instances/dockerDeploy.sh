5e4eb32ba2fad917fd0008022bd89b0865fa3649
Juan Zurano <jgzurano@interactar.com>
2016-06-22 05:57:54 +0000

    removal of untagged images

--- instances/dockerDeploy.sh
+++ instances/dockerDeploy.sh
@@ -11,6 +11,8 @@ echo "stopping docker-$dockerEnv-compose.yml"
 $compose -f docker-$dockerEnv-compose.yml pull
 $compose -f docker-$dockerEnv-compose.yml down 
 for i in $(docker ps -a|cut -d ' ' -f1|grep -v CONTA) ; do docker rm $i ; done
+echo "cleaning untagged images"
+for i in $(docker images | grep "<none>" | awk "{print \$3}") ; do docker rmi $i ; done
 echo "starting docker-$dockerEnv-compose.yml"
 #I'm not interested in stdout. Hopefully I ll configure using other way.
 #(nohup $compose -f docker-$dockerEnv-compose.yml up > /dev/null 2>&1 &)
