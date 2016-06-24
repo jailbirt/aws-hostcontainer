733acc8f2f78acdca67f3f6aa59875a556280b40
Juan Zurano <jgzurano@interactar.com>
2016-06-24 06:35:49 +0000

    fix cleanup

--- docker/dockerCleanUp.sh
+++ docker/dockerCleanUp.sh
@@ -2,8 +2,8 @@ echo killing dockers
 for i in $(docker ps|tr -s " "|cut -d' ' -f1|grep -v CON); do echo killing $i && docker kill $i ; done
 echo deleting dockers
 for i in $(docker ps|tr -s " "|cut -d' ' -f1|grep -v CON); do echo deleting $i && docker rm -f $i ; done
-echo deleting images
-for i in $(docker images|tr -s " "|cut -d' ' -f3|grep -v IMAGE); do echo Deleting $i && docker rmi -f $i ; done
+echo cleaning untagged images
+for i in $(docker images | grep "<none>" | awk "{print \$3}") ; do docker rmi -f $i ; done
 echo Cleaning unused docker volumes
 rm -fr /var/lib/docker/volumes/* /var/run/docker*
 service docker restart
