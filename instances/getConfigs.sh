44b255615cfdba25a2de8f5f012611b2574df60a
Juan Zurano <jgzurano@interactar.com>
2016-06-27 17:06:34 +0000

    perms fix for configs

--- instances/getConfigs.sh
+++ instances/getConfigs.sh
@@ -3,5 +3,5 @@ scriptsPath='/home/ubuntu/aws-hostcontainer'
 aws='/usr/bin/aws'
 source $scriptsPath/instances/instanceVars.sh --printvars
 echo Get Your Configurations $aws s3 sync s3://$configurationsBucket /home/ubuntu/configs
-$aws s3 sync s3://$configurationsBucket /home/ubuntu/configs
+sudo $aws s3 sync s3://$configurationsBucket /home/ubuntu/configs --region $region
 sudo chown -R ubuntu.ubuntu /home/ubuntu/configs
