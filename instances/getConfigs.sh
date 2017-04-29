94f96790f6b74ff65053c2ed128c1ba52bb08518
Juan Zurano <jgzurano@interactar.com>
2017-04-29 04:45:15 +0000

    +x for scripts

--- instances/getConfigs.sh
+++ instances/getConfigs.sh
@@ -5,3 +5,4 @@ source $scriptsPath/instances/instanceVars.sh --printvars
 echo Get Your Configurations $aws s3 sync s3://$configurationsBucket /home/ubuntu/configs
 sudo $aws s3 sync s3://$configurationsBucket /home/ubuntu/configs --region $region
 sudo chown -R ubuntu.ubuntu /home/ubuntu/configs
+find /home/ubuntu/configs -type f -iname "*.sh" -exec chmod a+x {} +
