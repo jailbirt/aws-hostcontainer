a9f88f6a9315b560bce609ec0cf4d91e7f89f6e6
Juan Zurano <jgzurano@interactar.com>
2017-09-11 17:02:44 +0000

    elbv2 script

--- instances/addInstanceToBalancer.sh
+++ instances/addInstanceToBalancer.sh
@@ -4,7 +4,21 @@ source /home/ubuntu/aws-hostcontainer/instances/instanceVars.sh
 
 host=$(hostname -s)
 
+echo $host | grep -qie "\w01$"
+status=$?
+if [ "$status" -eq 0 ]
+then
+  loadBalancer="$geoserverLoadBalancerWrite"
+else
+  loadBalancer="$geoserverLoadBalancer"
+fi
+
 for balancer in $loadBalancer;do
-  echo "aws elb register-instances-with-load-balancer --load-balancer-name $balancer --instances $instanceID --region $region" debug $balancer
-  aws elb register-instances-with-load-balancer --load-balancer-name $balancer --instances $instanceID --region $region
+  echo "aws elbv2 register-targets --target-group-arn $balancer --targets Id=$instanceID"
+  aws elbv2 register-targets --target-group-arn $balancer --targets Id=$instanceID --region $region
 done
+
+#for balancer in $loadBalancer;do
+#  echo "aws elb register-instances-with-load-balancer --load-balancer-name $balancer --instances $instanceID --region $region" debug $balancer
+#  aws elb register-instances-with-load-balancer --load-balancer-name $balancer --instances $instanceID --region $region
+#done
