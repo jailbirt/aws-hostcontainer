c3341f34448377059734c6d3e0c01513d6a67ad3
Ubuntu <ubuntu@awsgeoclusters03.webgis.frontec.net>
2017-09-26 00:47:19 +0000

    aws fix

--- instances/getConfigs.sh
+++ instances/getConfigs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scriptsPath='/home/ubuntu/aws-hostcontainer'
-aws='/usr/bin/aws'
+aws=$(which aws)
 source $scriptsPath/instances/instanceVars.sh --printvars
 echo Get Your Configurations $aws s3 sync s3://$configurationsBucket /home/ubuntu/configs
 sudo $aws s3 sync s3://$configurationsBucket /home/ubuntu/configs --region $region
