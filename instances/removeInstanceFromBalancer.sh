892047ad643f0f69d01bf90331d3dc0d7cc78a8d
Ubuntu <ubuntu@awsgeoclusterp02.webgis.frontec.net>
2018-01-06 22:39:11 +0000

    elbv2 deregister script

--- instances/removeInstanceFromBalancer.sh
+++ instances/removeInstanceFromBalancer.sh
@@ -2,8 +2,19 @@
 
 source /home/ubuntu/aws-hostcontainer/instances/instanceVars.sh
 
+host=$(hostname -s)
+
+echo $host | grep -qie "\w01$"
+status=$?
+if [ "$status" -eq 0 ]
+then
+  loadBalancer="$geoserverLoadBalancerWrite"
+else
+  loadBalancer="$geoserverLoadBalancer"
+fi
+
 for balancer in $loadBalancer
 do
-  echo "Removing Instance: $instanceID from Balancer: $balancer"
-  aws elb deregister-instances-from-load-balancer --load-balancer-name $balancer --instances $instancesID
+  echo "aws elbv2 deregister-targets --target-group-arn $balancer --targets Id=$instanceID --region $region"
+  aws elbv2 deregister-targets --target-group-arn ${balancer} --targets Id=${instanceID} --region ${region}
 done
